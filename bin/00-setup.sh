a7521081a80e637ca253a500921af5da7eea1984
duktus <duktus@mdnh.xyz>
2024-03-18 17:50:11 +0100

    Rewrote most parts
    - grouping packages for better structure
    - added new packages for backups, system monitoring
    - testing incus support

--- bin/00-setup.sh
+++ bin/00-setup.sh
@@ -2,49 +2,52 @@
 
 set -euo pipefail
 
+# Remove unnecessary packages before installation.
+rm -rf /etc/yum.repos.d/google-chrome.repo
+
+# base setup:
+# - remove unnecessary packages.
+# - gnome-extensions with external dependencies.
+# - distrobox as alternative to toolbox.
+# - replace power-profiles-daemon with tuned.
+# - gnome tweaks and popos icons for desktop customization.
+rpm-ostree override remove firefox firefox-langpacks gnome-tour power-profiles-daemon
+rpm-ostree install distrobox gnome-shell-extension-appindicator gnome-shell-extension-gsconnect \
+    gnome-tweaks pop-icon-theme tuned tuned-ppd tuned-utils tuned-profiles-atomic
+
+# monitoring and system administration:
+# - bottom, lm_sensors, iotop, nvtop, ncdu, powertop for system monitoring.
+# - setroubleshoot-server enables SELinux troubleshooting in flatpacked Cockpit.
+# - intel-undervolt as a desperate attempt to improve my shitty battery life.
+rpm-ostree install bottom lm_sensors iotop ncdu nvtop powertop setroubleshoot-server intel-undervolt
+
+# mostly used for development:
+# - vscode as default editor.
+# - virtualization via libvirt + qemu and virt-manager as a simple GUI.
+# - podman-compose for docker-compose compatibility.
+# - incus + distrobuilder for system containers and virtual machines.
+# - podmansh for containerized loginshells.
+# - vargant for easy vm setups -- may become obsolete with incus.
+rpm-ostree install code incus incus-agent distrobuilder libvirt libvirt-dbus podman-compose podmansh virt-install virt-manager vagrant vagrant-libvirt qemu qemu-kvm
+
+# some extra utilites:
+# - rsync, rclone, restic & autorestic for backups & cloud syncing.
+# - tailscale & wireguard for vpn support.
+# - cryfs for encryption -- enables usage of vaults flatpak.
+rpm-ostree install cryfs rclone rsync restic autorestic tailscale wireguard-tools
+
+# remove unnecessary repos after installation,
+# as updates will be handled by building the image.
+rm -rf /etc/yum.repos.d/{tailscale,vscode,ganto-lx4c,atim-bottom}.repo
+
+# enable automatic updates.
 sed -i 's/^#AutomaticUpdatePolicy=none$/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
 systemctl enable rpm-ostreed-automatic.timer
 
-# remove unncessary google-chrome.
-rm -rf /etc/yum.repos.d/google-chrome.repo
-
-# ensure firewalld is enabled
+# enable systemlevel services:
 systemctl enable firewalld.service
-
-# enable podman system socket
-systemctl enable podman.socket
-
-# remove unnecessary packages
-rpm-ostree override remove firefox firefox-langpacks gnome-tour
-
-# install packages
-rpm-ostree install \
-    alacritty \
-    adw-gtk3-theme \
-    cryfs \
-    distrobox \
-    gnome-shell-extension-appindicator \
-    gnome-shell-extension-gsconnect \
-    gnome-tweaks \
-    libvirt \
-    libvirt-dbus \
-    virt-install \
-    virt-manager \
-    vagrant \
-    vagrant-libvirt \
-    qemu \
-    qemu-kvm \
-    ncdu \
-    pop-icon-theme \
-    rsync \
-    rclone \
-    setroubleshoot-server \
-    tailscale \
-    wireguard-tools
-
-# remove uncessary tailscale repo
-rm -rf /etc/yum.repos.d/tailscale.repo
-
-# Enable virzualization
 systemctl enable libvirtd.service
+systemctl enable podman.socket
 systemctl enable tailscaled.service
+# systemctl enable tuned.service
+# systemctl enable swtpm-workaround.service

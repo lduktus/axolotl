afac52570890b217d41d90b1f9b01df129c7cd39
duktus <duktus@mdnh.xyz>
2024-05-04 15:17:59 +0200

    Removed packages
    - distrobox (I currently don't use it)
    - nvtop (does not work with my non dedicated gpu)
    - distrobuilder (I get a build error after upgrading the OS, but don't really require it currently)
    - pop icons

--- bin/00-setup.sh
+++ bin/00-setup.sh
@@ -8,33 +8,31 @@ rm -rf /etc/yum.repos.d/google-chrome.repo
 # base setup:
 # - remove unnecessary packages.
 # - gnome-extensions with external dependencies.
-# - distrobox as alternative to toolbox.
 # - replace power-profiles-daemon with tuned.
 # - gnome tweaks and popos icons for desktop customization.
 rpm-ostree override remove firefox firefox-langpacks gnome-tour power-profiles-daemon
-rpm-ostree install distrobox gnome-shell-extension-appindicator gnome-shell-extension-gsconnect \
-    gnome-tweaks pop-icon-theme tuned tuned-ppd tuned-utils tuned-profiles-atomic
+rpm-ostree install bash-completion gnome-shell-extension-appindicator gnome-shell-extension-gsconnect \
+    gnome-tweaks tuned tuned-ppd tuned-utils tuned-profiles-atomic
 
 # monitoring and system administration:
-# - bottom, lm_sensors, iotop, nvtop, ncdu, powertop for system monitoring.
+# - bottom, lm_sensors, iotop, ncdu, powertop for system monitoring.
 # - setroubleshoot-server enables SELinux troubleshooting in flatpacked Cockpit.
-# - intel-undervolt as a desperate attempt to improve my shitty battery life.
-rpm-ostree install bottom lm_sensors iotop ncdu nvtop powertop setroubleshoot-server intel-undervolt
+rpm-ostree install bottom lm_sensors iotop ncdu powertop setroubleshoot-server
 
-# mostly used for development:
-# - vscode as default editor.
+# development and virtualization:
+# - vscode as editor.
 # - virtualization via libvirt + qemu and virt-manager as a simple GUI.
 # - podman-compose for docker-compose compatibility.
-# - incus + distrobuilder for system containers and virtual machines.
+# - incus for system containers and virtual machines.
 # - podmansh for containerized loginshells.
 # - vargant for easy vm setups -- may become obsolete with incus.
-rpm-ostree install code incus incus-agent distrobuilder libvirt libvirt-dbus podman-compose podmansh virt-install virt-manager vagrant vagrant-libvirt qemu qemu-kvm
+rpm-ostree install code incus incus-agent libvirt libvirt-dbus podman-compose podmansh virt-install virt-manager vagrant vagrant-libvirt qemu qemu-kvm
 
 # some extra utilites:
-# - rsync, rclone, restic & autorestic for backups & cloud syncing.
-# - tailscale & wireguard for vpn support.
-# - cryfs for encryption -- enables usage of vaults flatpak.
-rpm-ostree install cryfs rclone rsync restic autorestic tailscale wireguard-tools
+# - rsync, rclone for backups & cloud syncing.
+# - tailscale & wireguard for VPN support.
+# - cryfs for encryption -- enables usage of Vaults via flatpak.
+rpm-ostree install cryfs rclone rsync tailscale wireguard-tools
 
 # remove unnecessary repos after installation,
 # as updates will be handled by building the image.
@@ -49,5 +47,5 @@ systemctl enable firewalld.service
 systemctl enable libvirtd.service
 systemctl enable podman.socket
 systemctl enable tailscaled.service
-# systemctl enable tuned.service
+# systemctl enable /usr/lib/systemd/system/tuned.service
 # systemctl enable swtpm-workaround.service

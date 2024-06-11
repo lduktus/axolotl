e5b06b179fbb622f5415ccddc42cdb58631ffee7
duktus <duktus@mdnh.xyz>
2024-06-12 00:53:43 +0200

    heavily refactored - installing all packages in one action - installing homebrew

--- bin/00-setup.sh
+++ bin/00-setup.sh
@@ -1,51 +1,71 @@
-#/usr/bin/env bash
+#/usr/bin/bash
 
 set -euo pipefail
 
-# Remove unnecessary packages before installation.
-rm -rf /etc/yum.repos.d/google-chrome.repo
-
 # base setup:
-# - remove unnecessary packages.
-# - gnome-extensions with external dependencies.
-# - replace power-profiles-daemon with tuned.
-# - gnome tweaks and popos icons for desktop customization.
-rpm-ostree override remove firefox firefox-langpacks gnome-tour power-profiles-daemon
-rpm-ostree install bash-completion gnome-shell-extension-appindicator gnome-shell-extension-gsconnect \
-    gnome-tweaks tuned tuned-ppd tuned-utils tuned-profiles-atomic
+rpm-ostree override remove \
+		firefox \
+		firefox-langpacks \
+		gnome-tour \
+		yelp \
+		gnome-shell-extension-background-logo \
+			--install borgbackup \
+			--install borgmatic \
+			--install rclone \
+			--install zstd \
+            --install firewall-config \
+			--install tailscale \
+            --install wireguard-tools \
+			--install code \
+			--install distrobox \
+			--install podman-compose \
+			--install podmansh \
+			--install edk2-ovmf \
+			--install incus \
+			--install libvirt \
+			--install libvirt-dbus \
+			--install qemu \
+			--install qemu-kvm \
+			--install swtpm-tools \
+			--install virt-install \
+			--install virt-manager \
+			--install virt-viewer \
+			--install htop \
+			--install lm_sensors \
+			--install ncdu \
+			--install powertop \
+			--install smartmontools \
+			--install setroubleshoot-server \
+			--install adw-gtk3-theme \
+			--install intel-one-mono-fonts \
+			--install jetbrains-mono-fonts-all \
+			--install gnome-shell-extension-appindicator \
+			--install gnome-shell-extension-caffeine \
+			--install gnome-shell-extension-dash-to-dock \
+			--install gnome-tweaks
 
-# monitoring and system administration:
-# - bottom, lm_sensors, iotop, ncdu, powertop for system monitoring.
-# - setroubleshoot-server enables SELinux troubleshooting in flatpacked Cockpit.
-rpm-ostree install bottom lm_sensors iotop ncdu powertop setroubleshoot-server
-
-# development and virtualization:
-# - vscode as editor.
-# - virtualization via libvirt + qemu and virt-manager as a simple GUI.
-# - podman-compose for docker-compose compatibility.
-# - incus for system containers and virtual machines.
-# - podmansh for containerized loginshells.
-# - vargant for easy vm setups -- may become obsolete with incus.
-rpm-ostree install code incus incus-agent libvirt libvirt-dbus podman-compose podmansh virt-install virt-manager vagrant vagrant-libvirt qemu qemu-kvm
-
-# some extra utilites:
-# - rsync, rclone for backups & cloud syncing.
-# - tailscale & wireguard for VPN support.
-# - cryfs for encryption -- enables usage of Vaults via flatpak.
-rpm-ostree install cryfs rclone rsync tailscale wireguard-tools
-
-# remove unnecessary repos after installation,
-# as updates will be handled by building the image.
-rm -rf /etc/yum.repos.d/{tailscale,vscode,ganto-lx4c,atim-bottom}.repo
+# remove unnecessary repos after installation:
+rm -rf /etc/yum.repos.d/{tailscale,vscode,ganto-lx4c,google-chrome}.repo
 
 # enable automatic updates.
 sed -i 's/^#AutomaticUpdatePolicy=none$/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
-systemctl enable rpm-ostreed-automatic.timer
 
-# enable systemlevel services:
 systemctl enable firewalld.service
-systemctl enable libvirtd.service
+systemctl enable rpm-ostreed-automatic.timer
 systemctl enable podman.socket
+
+systemctl enable libvirtd.service
+systemctl enable lxcfs.service
+systemctl enable incus.service
 systemctl enable tailscaled.service
-# systemctl enable /usr/lib/systemd/system/tuned.service
-# systemctl enable swtpm-workaround.service
+
+# brew installation
+# see: https://github.com/ublue-os/bluefin/blob/6d144f14817033a6ca5dd0a5edfb9f2371b001bb/build_files/base/brew.sh#L12
+mkdir -p /var/home
+mkdir -p /var/roothome
+touch /.dockerenv
+curl -Lo /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+chmod +x /tmp/brew-install
+/tmp/brew-install
+tar --zstd -cvf /usr/share/homebrew.tar.zst /home/linuxbrew/.linuxbrew
+rm -rf /var/home /home/linuxbrew .dockerenv
